1375aa583aa9ef8043bb670758b1c4d2c0c59650
Matthew Kanemy <mkanemy@uwaterloo.ca>
2023-08-22 22:07:23 -0400

    Add intercept

--- cypress/e2e/App.cy.js
+++ cypress/e2e/App.cy.js
@@ -1,12 +1,16 @@
 describe('template spec', () => {
-  Cypress.config('defaultCommandTimeout', 10000);
-  it('passes', () => {
+  beforeEach(() => {
     cy.intercept('/root', {
       statusCode: 200,
       data: ["were here!"],
     }).as("root")
-
+    
+    cy.intercept({pathname: '**/+page.svelte*'}).as('svelte')
     cy.visit('localhost:3000');
+    cy.wait('@svelte')
+  })
+
+  it('passes', () => {    
     cy.contains('were here!');
   })
 })
