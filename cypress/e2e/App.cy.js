1ae660be90455e5a5dbec0b6f23749d5f11780c3
Matthew Kanemy <mkanemy@uwaterloo.ca>
2023-08-22 21:20:05 -0400

    Fix cypress tests & config

--- cypress/e2e/App.cy.js
+++ cypress/e2e/App.cy.js
@@ -1,7 +1,7 @@
 describe('template spec', () => {
   it('passes', () => {
-    cy.visit(Cypress.config().baseUrl());
-    cy.contains('We made it :D');
+    cy.visit('localhost:3000');
+    cy.contains('We made it! :D');
     cy.get('.App-link').click();
     cy.contains('React');
   })
