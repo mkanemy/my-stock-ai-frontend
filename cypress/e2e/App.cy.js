149726b4aad1e718ca2aa6803711412c82ebe32f
Matthew Kanemy <mkanemy@uwaterloo.ca>
2023-08-23 19:37:24 -0400

    fix cy test

--- cypress/e2e/App.cy.js
+++ cypress/e2e/App.cy.js
@@ -1,12 +1,11 @@
 describe('template spec', () => {
   it('passes', () => {
-    cy.intercept('/root', {
+    cy.intercept('root', {
       statusCode: 200,
-      data: ["were here!"],
+      body: ["were here!"],
     }).as("root")
 
     cy.visit('localhost:3000');
-    // cy.contains('were here!');
-    cy.contains('Learn React');
+    cy.contains('were here!');
   })
 })
