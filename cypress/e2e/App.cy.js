15bca416092dea82f30e7c2455f15a67e0a64896
Matthew Kanemy <mkanemy@uwaterloo.ca>
2023-08-22 20:57:32 -0400

    run on dif env

--- cypress/e2e/App.cy.js
+++ cypress/e2e/App.cy.js
@@ -1,6 +1,6 @@
 describe('template spec', () => {
   it('passes', () => {
-    cy.visit('localhost:3000');
+    cy.visit(Cypress.config().baseUrl());
     cy.contains('We made it :D');
     cy.get('.App-link').click();
     cy.contains('React');
