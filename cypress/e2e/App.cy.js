c630a73b4e54f57c096463f4acd4e25c02039713
Matthew Kanemy <mkanemy@uwaterloo.ca>
2023-08-22 22:01:34 -0400

    Fix cypress tests to mock

--- cypress/e2e/App.cy.js
+++ cypress/e2e/App.cy.js
@@ -1,14 +1,11 @@
 describe('template spec', () => {
-  Cypress.config('defaultCommandTimeout', 20000);
-
   it('passes', () => {
+    cy.intercept('/root', {
+      statusCode: 200,
+      body: ["were here!"],
+    }).as("root")
+
     cy.visit('localhost:3000');
-    cy.intercept('GET', '*root*', {
-      statusCode: 201,
-      data: ["hi"],
-    })
-    cy.contains('We made it! :D');
-    cy.get('.App-link').click();
-    cy.contains('React');
+    cy.contains('were here!');
   })
 })
