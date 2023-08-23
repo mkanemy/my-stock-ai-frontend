83c6a0ae86a59447731408225fb1962c6d37b9f7
Matthew Kanemy <mkanemy@uwaterloo.ca>
2023-08-23 19:45:43 -0400

    See screenshots

--- cypress/e2e/App.cy.js
+++ cypress/e2e/App.cy.js
@@ -1,12 +1,9 @@
 describe('template spec', () => {
   it('passes', () => {
-    cy.wait(1000)
-    cy.intercept('/dummy').as('dummy')
     cy.intercept('root', {
       statusCode: 200,
       body: ["were here!"],
     }).as("root")
-    cy.wait(1000)
 
     cy.visit('localhost:3000');
     cy.contains('were here!');
