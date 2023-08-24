5537702d62a7493137c3e12155b288844e194564
Matthew Kanemy <mkanemy@uwaterloo.ca>
2023-08-23 20:10:22 -0400

    remove useEffect

--- src/views/App.js
+++ src/views/App.js
@@ -1,6 +1,6 @@
 import logo from '../misc/logo.svg';
 import '../styles/App.css';
-import { React, useState, useEffect } from 'react';
+import { React, useState } from 'react';
 import axios from 'axios';
 
 function App() {
