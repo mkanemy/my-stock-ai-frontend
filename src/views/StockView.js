eebc49cf8f5ccc84da2abc86bdddff09519f8508
Matthew Kanemy <mkanemy@uwaterloo.ca>
2023-09-04 16:48:27 -0400

    Add the stock page

--- src/views/StockView.js
+++ src/views/StockView.js
@@ -1,29 +1,18 @@
+import '../styles/StockStyle.css';
 import { React, useState, useEffect } from 'react';
 import { useLocation } from 'react-router-dom';
-import axios from 'axios';
+import StockCompanyDataView from '../components/StockCompanyDataView'
 
 function StockView() {
 
     const {state} = useLocation();
     const { stock } = state;
 
-    const [response, setResponse] = useState();
-
-    useEffect(() => {
-    axios.get(process.env.REACT_APP_BACKEND_URL + 'stock/details', { params: { ticker: stock } })
-        .then(res => {
-            setResponse(res.data);
-        })
-        .catch(error => {
-            setResponse("error");
-            console.error(error);
-        });
-    }, []);
-
     return (
         <div className="StockView">
-            {stock}
-            {JSON.stringify(response)}
+            <StockCompanyDataView stock={stock}/>
+            {/* <StockQuoteDataView data={response.financialMetrics}/> */}
+            {/* <StockAdditionalDataView data={response.quoteData}/> */}
         </div>
     );
 }
